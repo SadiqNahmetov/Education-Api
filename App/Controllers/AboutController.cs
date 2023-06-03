2a92475519be8fd67d59d20b91bc861d80028eb1
SadiqNahmetov <sadigfn@code.edu.az>
2023-06-03 15:33:59 +0400

    FIxed About Get GetAll

---
+++ App/Controllers/AboutController.cs
@@ -0,0 +1,52 @@
+﻿using DomainLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.DTOs.About;
+using ServiceLayer.Services.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Controllers
+{
+    public class AboutController : AppController
+    {
+        private readonly IAboutService _aboutService;
+        private readonly IWebHostEnvironment _env;
+
+        public AboutController(IAboutService aboutService, IWebHostEnvironment env)
+        {
+            _aboutService = aboutService;
+            _env = env;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get([Required] int id)
+        {
+            try
+            {
+                return Ok(await _aboutService.GetAsync(id));
+            }
+            catch (NullReferenceException)
+            {
+
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                return Ok(await _aboutService.GetAllAsync());
+            }
+            catch (NullReferenceException)
+            {
+                return NotFound();
+            }
+
+        }
+
+      
+    }
+}
